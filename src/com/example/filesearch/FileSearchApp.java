529ff57662dc1502c9d5d8db0050d8f7547b4767
Danish Markatia <dmarkatia@knights.ucf.edu>
2017-03-17 20:10:30 -0400

    This is my second commit

--- src/com/example/filesearch/FileSearchApp.java
+++ src/com/example/filesearch/FileSearchApp.java
@@ -1,44 +1,86 @@
 package com.example.filesearch;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class FileSearchApp {
 
 	String path;
 	String regex;
 	String zipFileName;
-	
+
 	public static void main(String[] args) {
 		FileSearchApp app = new FileSearchApp();
-		
-		switch (Math.min(args.length, 3)){
+
+		switch (Math.min(args.length, 3)) {
 		case 0:
 			System.out.println("USAGE: FileSearchApp path [regex] [zipfile]");
 			return;
-		case 3: app.setZipFileName(args[2]);
-		case 2: app.setRegex(args[1]);
-		case 1: app.setPath(args[0]);
+		case 3:
+			app.setZipFileName(args[2]);
+		case 2:
+			app.setRegex(args[1]);
+		case 1:
+			app.setPath(args[0]);
 		}
-		
+
 		try {
-			app.walkDirectory(app.getPath());
+			app.walkDirectoryJava6(app.getPath());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	public void walkDirectory(String path){
+
+	public void walkDirectory(String path) {
 		System.out.println("walkDirectory: " + path);
 		searchFile(null);
 		addFileToZip(null);
 	}
-	
-	public void searchFile(File file){
+
+	public void walkDirectoryJava6(String path) {
+		File dir = new File(path);
+		File[] files = dir.listFiles();
+
+		for (File file : files) {
+			if (file.isDirectory()) {
+				walkDirectoryJava6(file.getAbsolutePath());
+			} else {
+				processFile(file);
+			}
+		}
+	}
+
+	public void walkDirectoryJava7(String path) throws IOException {
+		Files.walkFileTree(Paths.get(path), new SimpleFileVisitor<Path>() {
+			@Override
+			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) 
+					throws IOException {
+				processFile(file.toFile());
+				return FileVisitResult.CONTINUE;
+			}
+		});
+	}
+
+	public void walkDirectoryJava8(String path) throws IOException {
+		Files.walk(Paths.get(path)).forEach(f -> processFile(f.toFile()));
+	}
+
+	public void processFile(File file) {
+		System.out.println("processFile: " + file);
+	}
+
+	public void searchFile(File file) {
 		System.out.println("searchFile: " + file);
 	}
-	
-	public void addFileToZip(File file){
+
+	public void addFileToZip(File file) {
 		System.out.println("addFileToZip: " + file);
 	}
 
